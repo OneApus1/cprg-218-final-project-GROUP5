00306e285c44893c969349e9afd8aa58d7d9a955
Cll4 <eyan_delisle@yahoo.ca>
2024-08-14 16:13:28 -0600

    Added some flex box properties to the screenshots, also changed the clickable photos on the second page to loop correctly

--- scripts/eventListener.js
+++ scripts/eventListener.js
@@ -2,12 +2,12 @@ const firstImage = document.querySelector("img");
 
 function switchImage() {
     let MySrc = firstImage.getAttribute("src");
-    if (MySrc === "media/Parrapa Cover.png") {
+    if (MySrc === "media/Joe Chin.png") {
         firstImage.setAttribute("src", "media/PJ.png");
     } else if (MySrc === "media/PJ.png") {
         firstImage.setAttribute("src", "media/Katy.png");
     } else {
-        firstImage.setAttribute("src", "media/Parrapa Cover.png");
+        firstImage.setAttribute("src", "media/Joe Chin.png");
     }
 };
 
